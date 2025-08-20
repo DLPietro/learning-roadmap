e5d919905fc1e3aa62d313a6c0869a0dc7b79dbc
Pietro Di Leo <90868226+DLPietro@users.noreply.github.com>
2025-08-20 16:58:26 +0200

    test: correct data extraction from yfinance and refactor return calculation logic
    test : correct data extraction from yfinance and refactor return calculation logic
    
    add volatility and annualized volatility calculation on the fuction
    
    to be tested

--- week-1/day_2_functions_annualized_return.py
+++ week-1/day_2_functions_annualized_return.py
@@ -1,28 +1,36 @@
 ### DAY 1: DOWNLOAD AND EXERCIZE ON FINANCIAL DATA
-================
+
 # MAIN GOALS
 # Learning how to use Python's functions
 # Calculating annualized returns from daily returns - Formula = (1 + Daily return)^252 - 1
 # Applying the function to more Tickers
-================
+
 
 # Step 1: Installing & importaing Libraries (Yahoo Finance, Pandas)
-!pip install yfinance, pandas            # Installing both with a single command
-import yfinance as yf, pandas as pd      # Importing them in a single row
+!pip install yfinance pandas            # Installing both with a single command
+import yfinance as yf, pandas as pd     # Importing them in a single row
 
 # Step 2: Download last 60 days of at least 3 Tickers 
-data = yf.download('GOOGL', 'AAPL', 'META')      # Selected assets: Apple, Google and Meta
-close_prices = list(data['Close'])               # Only Closing prices
-print(close_prices[-1])                          # Last prices available
+data = yf.download(['AAPL', 'MSFT', 'GOOGL'], period = '60d')      # Selected assets: Apple, Microsoft and Google for a peirod of 60 days
+close_prices = list(data['Close']['GOOGL', 'AAPL', 'META'])              # Only Closing prices
+print(close_prices[-1])                                             # Last prices available
 
-### EXERCISE 1: CALCULATING DAILY RETURNS (AS AT DAY 1) & ANNAUALISED RETURNS USING A SINGLE FORMULA
+### EXERCISE 1: CALCULATING DAILY RETURNS (AS AT DAY 1), ANNAUALISED RETURNS AND VOLATILITY USING A SINGLE FUNCTION
+def calc_daily_returns(prices):                # Function for daily return calculation
+  returns = []
+  for i in prices:                             # The loop uses the formula to calculate the daily returns, comparing the percentage of the first with the previous one
+    ret = (prices[i] / prices[i-1]) * 100      
+    returns.append(round(ret, 2))
+
 def calc_annualized_return(daily_returns):
-  daily_returns = []
-  for n in close_prices:
-    ret = (close_prices[n] / close_prices[n-1]) * 100      # The loop uses the formula to calculate the daily returns, comparing the percentage of the first with the previous one
-    daily_returns.append(round(ret, 2))
-  for n in daily_returns:
-    formula = ((1 + daily_returns) ** 252) - 1
-  print(daily_returns[:5])
+  daily_return_avg = sum(daily_returns) / len(daily_returns)
+  annualized_return = (1 + daily_return_avg) ** 252 - 1
+  volatility = daily_returns.std()
+  annualized_volatility = volatility * (252 * 0.5)
+  return {
+  "annualized_return": annualized_return,
+  "annualized_volatility": annualized_volatility
+}
 
-  
+
+

04bd259f103d9e46d1b5c2e277bcdb6362f08670
Pietro Di Leo <90868226+DLPietro@users.noreply.github.com>
2025-08-21 03:57:40 +0200

    refactor & perf: day1_python_basics.py
    refactor & perf: output of the script
    
    improve calculations verified via google colab

--- week-1/day1_python_basics.py
+++ week-1/day1_python_basics.py
@@ -1,44 +1,39 @@
 ### DAY 1: DOWNLOAD AND EXERCIZE ON FINANCIAL DATA
-
 # MAIN GOALS
-# Understanding data types, and first approach 
-
+# Understanding data types, and first approach
 
 # Step 1: Importing Yahoo Finance
-!pip install yfinance          # Command to download the Yahoo Finance library tool
-import yfinance as yf          # import the library to be used on the script
+#!pip install yfinance pandas         # Command to download the Yahoo Finance and Pandas library tool
+import yfinance as yf, pandas as pd      # import the library to be used on the script
+
 
 # Step 2: Download last 30 days of APPL Inc. Stock
-data = yf.download('AAPL', period = '30d')      # Select Asset and period
-close_prices = list(data['Close'])              # Closed prices list selected
-print(close_prices[:5])                         # Showing the first 5 rows / daily prices of the stock
+ticker = 'AAPL'                                 # Apple ticker selected
+data = yf.download(ticker, period = '30d')      # Select Asset and period
+close_prices = data['Close']                  # Closing prince selected
+print(close_prices[:4])                       # Showing the first 5 rows / daily prices of the stock
 
 ### EXERCISE 1: SAVING PRICES AND PRINT FIRST STATS
-min_price = min(close_prices)                      # Calculatin min, max and mean price
-max_price = max(close_prices)
-avg_price = sum(close_prices) / len(close_prices)
-ticker = 'AAPL'
+min_price = close_prices.min()                     # Calculatin min, max and mean price
+max_price = close_prices.max()
+avg_price = close_prices.mean()
+std_price = close_prices.std()
 
-stats = {"Ticker": ticker,                        # Print in a Single Variable the Stats
-    "Min": min_price,
-    "Max": max_price,
-    "Average": avg_price}
+stats = {"Ticker": ticker,
+          "Min": min_price,
+          "Max": max_price,
+          "Average": avg_price,
+          "Standard Deviation": std_price}
 print(stats)
 
 ### EXERCISE 2: COMPUTING DAILY RETURNS OF THE ASSET
-daily_returns = []                            # Creating a loop to insert the values in the list
-for n in close_prices[1:]:
-  ret = (close_prices[n] / close_prices[n-1]) * 100      # The loop uses the formula to calculate the daily returns, comparing the percentage of the first with the previous one
+daily_returns = []
+for i in range(1, len(close_prices)):
+  today = close_prices.iloc[i]
+  yesterday = close_prices.iloc[i-1]
+  ret = (((today - yesterday) / yesterday) * 100)
   daily_returns.append(round(ret, 2))
 
 print('Daily Returns', daily_returns[:5])              # It shows the result of the first 5 values
 
-### ALTERNATIVE EXERCISE 2: COMPUTING DAILY RETURNS OF THE ASSET (TO BE TESTED)
-# daily_returns = []
-# for i in range(1, len(close_prices)):
-  #   today_price = close_prices[i]
-   # yesterday_price = close_prices[i-1]
-# ret = ((today_price / yesterday_price) * 100)
-    # daily_returns.append(round(ret, 2))
-
-# print('Daily Returns', daily_returns[:5])              # It shows the result of the first 5 values
+print('Daily Returns', daily_returns[:5])              # It shows the result of the first 5 values

36078b0c9d0611eadd79e71911e1c9117a679052
Pietro Di Leo <90868226+DLPietro@users.noreply.github.com>
2025-09-01 20:43:31 +0200

    fix & feat: daily calculator and graph
    fix: daily calculator with pandas
    
    feat: graph for the daily returns

--- week-1/day_4_pandas_basics.py
+++ week-1/day_4_pandas_basics.py
@@ -4,36 +4,35 @@
 # 1) Creating a Dataframe with 3 assets with Close and Volume values of the last 60 days
 # 2) Calculating daily returns using pandas, in a new column to add in the dataframe (using .pct_change())
 # 3) Printing the first 10 values of the new column
-# 4) Creating a new column with 7 days moving average
+# 4) Creating a new column with 7-day moving average
 
-# Step 1: importing data and creating dataset
+# Step 1: importing data and creatinga  dataset
 # !pip install yfinance pandas     
-import yfinance as yf, pandas as pd, numpy as np
+import yfinance as yf, pandas as pd, numpy as np   # Importing them in a single row
 
-tickers = ['IWM', 'GLD', 'IGOV']                      # Defined tickets
-data = yf.download(tickers, period = '60d')           # downloading lst 60 days of data
-print(data.head())                                    # print the first values
-new_data = data['Close', 'Volume'][tickers]           # select close column
-print(new_data.head())                                # print first 5 rows of prices and market volumes values
+tickers = ['IWM', 'GLD', 'IGOV']                                   # Defined tickets
+data = yf.download(tickers, period = '60d')                        # Selected assets: Apple, Microsoft, and Google for a period of 60 days
+close_prices = data['Close'][['IWM', 'GLD', 'IGOV']]               # Only Closing prices
+volume = data['Volume'][['IWM', 'GLD', 'IGOV']]                    # Volume Column
+
+print(f"Last Prices Available: {close_prices.iloc[-1]}")           # Print last prices available
+print(f"Last Volumes Available: {volume.iloc[-1]}")                # Print Volume for Each Asset
+
 
 # Step 2-3-4: daily returns and analysis with pandas
-def analyse_returns(new_data, close_prices):
-    returns = new_data['Close'].pct_change()          # Function for daily returns from pandas
-    returns = returns.dropna()                        # remove first NaN value
-    avg_daily_return = returns.mean()                 # Average daily return
-    daily_volatility = returns.std()                           # Volatily
-    annualized_return = (1 + avg_daily_return) ** 252 -1       # Annlualized return
-    annualized_volatility = daily_volatility * np.sqrt(252)    # Annlualized volatility
-    moving_average = new_data['Close'].rolling(window = 7).mean()                                      # Moving Average (7 days)
-    # Results Dictionary
-    results = {
-        'Annualized Return': round(annualized_return, 4),
-        'Annualized Volatility': round(annualized_volatility, 4)
-    }
-    # Print Output
-    print(results)
-    print({'Daily Returns': returns.head(10)})
-    print({'Moving Average 7-Days': moving_average.head(10)})
-    
-    return returns, results, moving_average
-    
+def calc_daily_returns(prices):
+  """Calculate Daily Returns with pct_change()"""                               # Function for daily return calculation
+  returns = prices.pct_change()                                                 # Function ready using pandas
+  return returns
+
+calc_daily_returns(close_prices).dropna().head(10)                              # Print frist 10 rows
+
+
+### EXTRA STEP: GRAPH
+
+
+import matplotlib.pyplot as plt
+
+returns = calc_daily_returns(close_prices).dropna()
+returns.plot(figsize=(10,6), title="Daily Returns of IWM, GLD, IGOV")
+plt.show()
